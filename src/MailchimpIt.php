12b8d8213f1a48abf076b2814d20eb9ba1ef93b1
Vincent Loy <vincent.loy1@gmail.com>
2015-10-08 21:49:34 +0200

    remove useless stuff - Mailchimp API v3 ready

--- src/MailchimpIt.php
+++ src/MailchimpIt.php
@@ -8,57 +8,52 @@ require(dirname(__DIR__) . '/vendor/autoload.php');
 
 use \DrewM\MailChimp\MailChimp;
 
-try {
-    function mailchimpItError($message)
-    {
-        print_r(json_encode(array(
-            'status' => 'error',
-            'error' => $message
-        )));
-    }
+function mailchimpItError($message)
+{
+    print_r(json_encode(array(
+        'status' => 'error',
+        'error' => $message
+    )));
+}
 
-    if (!isset($_POST['newsletter_mail'])) {
+if (!isset($_POST['newsletter_mail'])) {
 
-        mailchimpItError('missing email parameter');
+    mailchimpItError('missing email parameter');
 
-    } else {
-        try {
-            $api_key = getenv('MAILCHIMP_API_KEY');
-            $list_id = getenv('MAILCHIMP_LIST_ID');
+} else {
+    try {
+        $api_key = getenv('MAILCHIMP_API_KEY');
+        $list_id = getenv('MAILCHIMP_LIST_ID');
 
-            $email = $_POST['newsletter_mail'];
-            $merge_vars = array();
+        $email = $_POST['newsletter_mail'];
+        $merge_vars = array();
 
-            if (isset($_POST['newsletter_first_name'])) {
-                $merge_vars['FNAME'] = $_POST['newsletter_first_name'];
-            }
-
-            if (isset($_POST['newsletter_last_name'])) {
-                $merge_vars['LNAME'] = $_POST['newsletter_last_name'];
-            }
-
-            $mailchimp = new MailChimp($api_key);
-
-            $result = $mailchimp->post(
-                'lists/' . $list_id . '/members',
-                array(
-                    'email_address' => $email,
-                    'status' => 'subscribed',
-                    'merge_fields' => $merge_vars
-                )
-            );
-
-            if ($result) {
-                print_r(json_encode($result));
-            } else {
-                mailchimpItError('Please check your configuration');
-            }
-
-        } catch (Exception $e) {
-            echo 'ertet';
-            mailchimpItError($e->getMessage());
+        if (isset($_POST['newsletter_first_name'])) {
+            $merge_vars['FNAME'] = $_POST['newsletter_first_name'];
         }
+
+        if (isset($_POST['newsletter_last_name'])) {
+            $merge_vars['LNAME'] = $_POST['newsletter_last_name'];
+        }
+
+        $mailchimp = new MailChimp($api_key);
+
+        $result = $mailchimp->post(
+            'lists/' . $list_id . '/members',
+            array(
+                'email_address' => $email,
+                'status' => 'subscribed',
+                'merge_fields' => $merge_vars
+            )
+        );
+
+        if ($result) {
+            print_r(json_encode($result));
+        } else {
+            mailchimpItError('Please check your configuration');
+        }
+
+    } catch (Exception $e) {
+        mailchimpItError($e->getMessage());
     }
-} catch (Exception $e){
-    echo $e->getMessage();
 }
